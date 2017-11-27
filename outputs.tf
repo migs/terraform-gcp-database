5d983bd5612ab1b4a884f582168820eb7923d98e
Stuart Moore <stuart.moore@gmail.com>
2017-11-27 15:44:59 +0000

    Add DB outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
-output "db-instance" {
+output "db-instance-name" {
   value = "${google_sql_database_instance.db-primary.name}"
+}
+
+output "db-instance-ip" {
+  value = "${google_sql_database_instance.db-primary.ip_address.0.ip_address}"
 }
