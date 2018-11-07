f82c0c7d73769d004de3bf2ba2fae129d5e14274
Developer Environment <vagrant@vagrant.vm>
2018-11-07 15:55:54 +0000

    Disable HA. Yes, this is a terrible commit.

--- database.tf
+++ database.tf
@@ -12,36 +12,12 @@ resource "google_sql_database_instance" "db-primary" {
       ]
     }
     backup_configuration {
-      binary_log_enabled = "true"
+      binary_log_enabled = "false"
       enabled = "true"
       start_time = "${var.db-backup-start_time}"
     }
     location_preference {
       zone = "${lookup(var.region_params["${var.region}"],"zone1")}"
-    }
-  }
-}
-
-resource "google_sql_database_instance" "db-failover" {
-  region = "${var.region}"
-  count = "${var.ha ? 1 : 0}"
-  database_version = "${var.db-version}"
-  master_instance_name = "${google_sql_database_instance.db-primary.name}"
-  replica_configuration {
-    failover_target = "true"
-    }
-  settings {
-    tier = "${var.db-tier}"
-    disk_autoresize = "true"
-    ip_configuration {
-      authorized_networks = [
-        { value = "${var.authorized_network_0}" },
-        { value = "${var.authorized_network_1}" },
-        { value = "${var.authorized_network_2}" },
-      ]
-    }
-    location_preference {
-      zone = "${lookup(var.region_params["${var.region}"],"zone2")}"
     }
   }
 }
